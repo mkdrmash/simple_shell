6189b07a121660fdae09090ecba42b55869a66d5
Mohammed Kedir <mkdrmash@gmail.com>
2022-09-09 02:24:57 -0700

    updated

--- memory.c
+++ memory.c
@@ -1,26 +1,91 @@
-#include "simpleshell.h"
+#include "shell.h"
 
 /**
- *_calloc -allocate memory for size bytes
- *@nmemb: number of element in the array
- *@size: bytes for each position in the array
- *Return: pointer void
+ * _realloc - reallocates memory block
+ * @ptr: pointer to the previous memory
+ * @old_size: the old size
+ * @new_size: the new size
+ *
+ * Return: a pointer to the newly allocated memory
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *p;
+	void *result;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	result = malloc(new_size);
+	if (result == NULL)
+		return (NULL);
+	if (ptr == NULL)
+	{
+		fill_an_array(result, '\0', new_size);
+		free(ptr);
+	}
+	else
+	{
+		_memcpy(result, ptr, old_size);
+		free(ptr);
+	}
+	return (result);
+}
+/**
+ * _memset - fills a memory with constant byte
+ * @s: pointer to memory area
+ * @n: first n bytes
+ * @byt: constant byte
+ *
+ * Return: A pointer to a character
+ */
+char *_memset(char *s, char byt, unsigned int n)
+{
 	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
+	for (i = 0; i < n; i++)
+	{
+		s[i] = byt;
+	}
+	return (s);
+}
+/**
+ * free_data - frees data
+ * @data: the data structure
+ *
+ * Return: (Success) positive number
+ * ------- (Fail) negative number
+ */
+int free_data(sh_t *data)
+{
+	free(data->line);
+	data->line = NULL;
+	free(data->args);
+	data->args = NULL;
+	free(data->cmd);
+	data->cmd = NULL;
+	free(data->error_msg);
+	data->error_msg = NULL;
+	return (0);
+}
+/**
+ * _memcpy - copy memory area
+ * @dest: Destination memory area
+ * @src: Source memory area
+ * @n: Amount of memory byte
+ *
+ * Return: A pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
 
-	p = malloc(nmemb * size);
-	if (p == NULL)
-		return (NULL);
-
-	for (i = 0; i < nmemb * size; i++)
-		p[i] = 0;
-
-	return (p);
-
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
